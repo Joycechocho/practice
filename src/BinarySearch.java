8546f4083f81ba782f22b1fd8b516b9a1f6ff3a1
Joyce Cho <joyce.cho@colorado.edu>
2018-11-13 09:42:08 -0700

    Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,10 @@
+/* Given a sorted array arr[] of n elements, write a function to search a given element x in arr[] */
+
+public class BinarySearch {
+
+    // return index of x if it is present in arr[], else return -1
+    void binarysearch(int[] arr, int x) {
+
+    }
+
+}
